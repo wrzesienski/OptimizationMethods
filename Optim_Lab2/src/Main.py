7affbe612ae91774b6d84d7b0ee7dc807f9b0a78
Wrzesienski <zhalig@inbox.ru>
2020-04-08 22:35:17 +0300

    v.1.1 upgraded StAnCo algorithm

--- Optim_Lab2/src/Main.py
+++ Optim_Lab2/src/Main.py
@@ -1,6 +1,30 @@
 # import numpy as np
 from Optim_Lab2.src.StandartAntColony import *
 
+class Comtrade:
+    """ класс записи результатов"""
+
+    def __init__(self):
+        self.alfa = []
+        self.betta = []
+        self.p = []
+        self.time = []
+        self.shortest_way = []
+
+    def set_coefficients(self, alfa, betta, p):
+        self.alfa.append(alfa)
+        self.betta.append(betta)
+        self.p.append(p)
+
+    def get_coefficients(self):
+        return self.alfa, self.betta, self.p
+
+    def record_results(self, time, way_length):
+        self.time.append(time)
+        self.shortest_way.append(way_length)
+
+    def show_results(self):
+        return self.time, self.shortest_way
 
 # матрица ребер графов
 way_matrix = np.load("C:/Users/Alexander/PythonProjects/"
@@ -10,13 +34,27 @@ way_matrix = np.load("C:/Users/Alexander/PythonProjects/"
 " 120 "
 print("матрица путей: \n", way_matrix)
 
-# создание двумерного списка феромонов
+# создание двухмерного списка феромонов
 ti = [[0 for i in range(len(way_matrix[0]))] for j in range(len(way_matrix[0]))]
 
+print("time: ", stopwatch(way_matrix, ti, 0.95, 1, 1))
 
 
-
-
-
-
-print("time: " ,stopwatch(way_matrix, ti, 0.95, 1, 1))
+# my_comtrade = Comtrade()
+#
+# a_b = [(i+1)/10 for i in range(0, 101, 20)]
+# p_list = [i/10 for i in range(0, 11, 2)]
+# counter = 0
+# for a in a_b:
+#     for b in a_b:
+#         for p in p_list:
+#             my_comtrade.set_coefficients(a, b, p)
+#
+#             time = stopwatch(way_matrix, ti, p, a, b)
+#             way = calc_shortest_way(way_matrix, ti)
+#             my_comtrade.record_results(time, way)
+#             counter += 1
+#             print(counter)
+#
+#
+# print(my_comtrade.time)
--- Optim_Lab2/src/StandartAntColony.py
+++ Optim_Lab2/src/StandartAntColony.py
@@ -1,84 +1,111 @@
 import time
 import random as rnd
 import numpy as np
+import matplotlib.pyplot as plt
 
-N = 100  # количество муравьев в популяции
-start_point = 0  # вершина начала маршрута
+N = 20  # количество муравьев в популяции
+start_point = 1  # вершина начала маршрута
 
 
 def calc_delta(graph_matrix, ti, alfa, betta):
-    """ рассчитывает приращение количества феромонов на
-    пройденных муравьями маршрутах"""
+    """ рассчитывает приращение значения феромонов на
+    пройденных муравьями маршрутах за один цикл"""
 
-    node_number = len(graph_matrix[0])  # количество узлов графа
+    node_number = len(graph_matrix[0])  # количество вершин графа
 
-    delta_ti = [[0 for i in range(node_number)] for j in range(node_number)]  # приращение
+    delta_ti = [[0 for i in range(node_number)] for j in range(node_number)]  # матрица приращения
 
-    def calc_sum(wway, tt, alfa, betta):
+    def sort_ways(s_route, s_way):
+        """ возвращает список потенциальных вершин, к которым
+        муравей может перебраться на данной итерации, т.е:
+        - функция отсекает те вершины, в которых муравей уже был
+        - функция отсекает вершины, с которыми у муравья нет связи"""
+        return [i for i in range(len(s_way)) if s_way[i] > 0 and i not in s_route]
 
-        summ = 0
+    def way_list(point, sort):
+        """ возвращает список с длиной маршрутов, к которым муравей
+        может добраться из данной вершины"""
+        return [graph_matrix[point][i] for i in sorted(sort) if i > 0]
 
-        for i in wway:
-            if i == 0: wway.remove(0)
+    def ti_list(point, tii):
+        """ возвращает список со значением феромонов маршрутов,
+        к которым муравей может добраться из данной вершины"""
+        return [ti[point][i] for i in sorted(tii) if i > 0]
 
+    def compare(a, b):
+        """сравнивает и возвращает числа в порядке возрастания"""
+        if a < b:
+            return a, b
+        return b, a
+
+    def calc_sum(wway, tt):
+
+        summary = 0
         for i in range(len(wway)):
-            summ += tt[i] ** alfa + 1 / (wway[i] ** betta)
+            summary += tt[i] ** alfa + 1 / (wway[i] ** betta)
 
-        return summ
+        return summary
 
-    def __is_this_way__(this_way, this_t, local_summ, alfa, betta):
+    def __is_chosen_way__(this_way, this_t, local_summ):
         """ возвращает значение True или False в зависимости от выпадения
         монетки """
 
         # вероятность перехода по маршруту
         probability = (this_t ** alfa + 1 / (this_way ** betta)) / local_summ
-
+        print("вероятность ", probability)
         # произойдет переход или нет
         penny = rnd.choices([0, 1], [1 - probability, probability])
 
         return penny
 
+
     for i in range(N):
 
         # вершина, в которой находится муравей/ сколько городов осталось пройти
-        actual_point, n = start_point, (node_number - 1)
+        actual_stop, n_left = start_point, (node_number - 1)
 
-        route_list = [actual_point]
+        route_list = [actual_stop]
         way_length = 0  # суммарная длина маршрута
 
-        while n > 0:  #
+        while n_left > 0:
+
+            potential_ways = list(graph_matrix[actual_stop])
+
+            # отсортированный список возможных вершин, к которым может добраться муравей
+            sort_route_pointer = sort_ways(route_list, potential_ways)
+
+            if not len(sort_route_pointer): break  # муравей зашел в тупик на последней итерации
+
+            "минус данного алгоритма: необходимо учесть добавление в \
+            суммарную длину маршрута того расстояния, которое пройдет муравей \
+            по возвращении в начальный город. Учитывается ниже"
+            if len(sort_route_pointer) and n_left == 1:
+                way_length += graph_matrix[sort_route_pointer[0]][start_point]
 
             # в случайном порядке выбираем потенциальное ребро
-            j = rnd.randint(0, node_number - 1)
+            next_stop = rnd.choice(sort_route_pointer)
             # берем значение его длины:
-            way = graph_matrix[actual_point][j]
+            way = graph_matrix[actual_stop][next_stop]
 
-            " FIXXXXXXX - возможность бесконечного зацикления!!!!!!!!!!!!!"
-            if way != 0 and j not in route_list:  # если связь есть и через вершину еще не проходили
+            # берем величину феромона ребра:
+            t_way = ti[actual_stop][next_stop]
 
-                # берем величину феромона ребра:
-                t_way = ti[actual_point][j]
-                rel_way_list = list(graph_matrix[actual_point])  # длины всех вершин, к которым можно добраться отсюда
-                rel_ferom_list = list(ti[actual_point])  # феромоны всех вершин, к которым можно добраться
+            way_on_pheromone = calc_sum(
+                                        way_list(actual_stop, sort_route_pointer),  # лист длин путей к соседям
+                                        ti_list(actual_stop, sort_route_pointer))  # лист феромонов на соседних путях
 
-                way_on_pheromone = calc_sum(rel_way_list, rel_ferom_list, alfa, betta)
+            if __is_chosen_way__(way, t_way, way_on_pheromone):
+                actual_stop = next_stop  # муравей переходит в новую вершину
+                route_list.append(next_stop)  # добавление в маршрутный лист
+                way_length += way
+                n_left -= 1
 
-                if __is_this_way__(way, t_way, way_on_pheromone, alfa, betta):
-                    actual_point = j  # муравей переходит в новую вершину
-                    route_list.append(j)  # добавление в маршрутный лист
-                    way_length += way
-                    n -= 1
-        # print("\n маршрут \n", route_list)
-
-        if graph_matrix[start_point][route_list[-1]] != 0:  # связь последней вершиной с первой есть
-            for i in range(len(route_list)):
-                yes = route_list[i]
-                before = route_list[i - 1]
-                if yes < before:  # заполняем матрицу выше главной диагонали
-                    delta_ti[yes][before] += node_number / way_length
-                else:
-                    delta_ti[before][yes] += node_number / way_length
-        # print("\n матрица приращения: \n", delta_ti)
+        else:  # выполняется в случае успешного прохождения по маршруту
+            for num in range(node_number):
+                j, k = compare(route_list[num], route_list[num - 1])
+                delta_ti[j][k] += node_number / way_length  # заполнение матрицы выше главной диагонали
+        print("\n маршрут \n", route_list)
+        print("\n матрица приращения: \n", delta_ti)
 
     return delta_ti
 
@@ -96,7 +123,8 @@ def calc_ti(ti, delta_ti, p):
 
 
 def calc_shortest_way(way_matrix, ti):
-    """расчет кратчайшего пути по актуальной матрице феромонов
+    """расчет прогнозируемого кратчайшего пути
+     по актуальной матрице феромонов
     :param way_matrix: матрица путей
     :param ti: матрица феромонов
     :return: кратчайший путь
@@ -104,10 +132,8 @@ def calc_shortest_way(way_matrix, ti):
 
     node_number = len(ti[0])
 
-    "FIXXXXX"
     init_mean = goal_mean = summary = 0
-    # goal_mean = 0
-    # suum = 0
+
     shortest_way = [int(init_mean)]
     for i in range(node_number - 1):
 
@@ -122,7 +148,9 @@ def calc_shortest_way(way_matrix, ti):
 
     summary += way_matrix[shortest_way[0]][shortest_way[-1]] # учет возврата в первый город
 
-    print("кратчайший путь на данный момент: ", shortest_way)
+    # print("кратчайший путь на данный момент: ", shortest_way)
+
+    # build_a_route(shortest_way)
 
     return summary
 
@@ -145,7 +173,26 @@ def stopwatch(way_matrix, ti, p, alfa, betta):
         # расчет кратчайшего пути по доминантным феромонам
         short = calc_shortest_way(way_matrix, ti)
 
-        print("\n итерация закончена")
-        print("кратчайший путь:", short)
+        # print("\n итерация закончена")
+        # print("кратчайший путь:", short)
 
     return time.perf_counter() - start_data
+
+
+def build_a_route(way):
+
+    X = [10, 10, 100, 100, 30, 20, 20, 50, 50, 85]
+    Y = [5, 85, 0, 90, 50, 55, 50, 75, 25, 50]
+
+    plt.title("Общий путь-", size=14)
+    X1=[X[way[i]] for i in np.arange(0,10,1)]
+    Y1=[Y[way[i]] for i in np.arange(0,10,1)]
+    plt.plot(X1, Y1, color='r', linestyle=' ', marker='o')
+    plt.plot(X1, Y1, color='b', linewidth=1)
+    X2=[X[way[10-1]],X[way[0]]]
+    Y2=[Y[way[10-1]],Y[way[0]]]
+    plt.plot(X2, Y2, color='g', linewidth=2,  linestyle='-', label='Путь от  последнего n к первому городу')
+    plt.legend(loc='best')
+    plt.grid(True)
+    plt.show()
+
